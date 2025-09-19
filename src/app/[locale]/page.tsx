b89e4332e4485c1e7cc7668e0557f85e3a1800d7
Dmytro Bondarchuk <dmitriy.bondarchuk@outlook.com>
2025-09-19 11:11:50 +0000

    Demo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,24 @@
-import {useTranslations} from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export default function IndexPage() {
-  const t = useTranslations('IndexPage');
-  return <h1>{t('title')}</h1>;
+  const t = useTranslations("IndexPage");
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <h2>
+        {t.rich("rich_escape", {
+          p: (c) => <p>{c}</p>,
+          i: (c) => <em>{c}</em>,
+          br: (_) => <br />,
+        })}
+      </h2>
+      <h2>
+        {t.rich("rich_not_enclosed_escape", {
+          p: (c) => <p>{c}</p>,
+          i: (c) => <em>{c}</em>,
+          br: (_) => <br />,
+        })}
+      </h2>
+    </div>
+  );
 }
